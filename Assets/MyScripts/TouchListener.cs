d7e6726ea212743fcd2bac231749e9d8e401d68e
Dan Flanagan <dan.flanagan93@gmail.com>
2015-11-16 21:37:24 -0500

    testing from computer

--- Assets/MyScripts/TouchListener.cs
+++ Assets/MyScripts/TouchListener.cs
@@ -15,6 +15,7 @@ public class TouchListener : MonoBehaviour {
     public Text charInput;
     public Text visibleDebug;
     public List<int> curInteractableButtons = new List<int>();
+    private RuntimePlatform device = Application.platform;
 
 	// Use this for initialization
 	void Start () {
@@ -30,57 +31,79 @@ public class TouchListener : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        visibleDebug.text = Input.touchCount.ToString();
-        if (Input.touchCount > 0)
+       
+
+        if(device == RuntimePlatform.Android || device == RuntimePlatform.IPhonePlayer)
         {
-            visibleDebug.text = "found a touch!";
-            Touch t = Input.touches[0];
-            isTouching = true;
-            info = Input.touches[0];
-
-            Vector3 screen_world = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
-            Vector2 touchPosition = new Vector2(screen_world.x, screen_world.y);
-
-            //listens for all interactions between the touch position and the box colliders
-            for (int i = 0; i < colliderlist.Count; i++)
+            if(Input.touchCount > 0)
             {
-                ButtonControls buttonInspector = buttonArray[i].GetComponent<ButtonControls>();
-                Collider2D temp = colliderlist[i];
-                if (temp == Physics2D.OverlapPoint(touchPosition))
-                {
-                    visibleDebug.text = "hit a button";
-                    Debug.Log("hit button" + buttonArray[i].name);
-                    if (curButton != -1)    //if this isnt the first button we have clicked
-                    {
-                        visibleDebug.text = "we have hit another button";
-                        //check to make sure that the only buttons we interact with are ones that are linked
-                        for (int j = 0; j < curInteractableButtons.Count; j++)
-                        {
-                            if (buttonInspector.number == curInteractableButtons[j])
-                            {
-                                charInput.text += buttonInspector.curChar;
-                                //we hit a button that we can touch
-                            }
-                        }
-                    }
-                    else
-                    {
-                        visibleDebug.text = "we hit our first button";
-                        curInteractableButtons = buttonInspector.interactableButtons;
-                        charInput.text = buttonInspector.curChar;
-                        curButton = buttonInspector.number;
-                        buttonInspector.sendClickedMessage();
-                    }
-                }
+                checkTouch(Input.touches[0].position);
             }
-            if (t.phase == TouchPhase.Ended)
-            { //if the touch has ended
+            else
+            {
+                visibleDebug.text = Input.touchCount.ToString();
+            }
 
-                isTouching = false;
-                charInput.text = "";
-                //checkTheString
+        }else if(device == RuntimePlatform.WindowsEditor)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                checkTouch(Input.mousePosition);
+            }
+            if (Input.GetMouseButtonUp(0))
+            {
+                visibleDebug.text = Input.touchCount.ToString();
             }
         }
+    }
+    
+    private void checkTouch(Vector2 pos)
+    {
+        visibleDebug.text = "found a touch!";
+        isTouching = true;
+
+        Vector3 screen_world = Camera.main.ScreenToWorldPoint(pos);
+        Vector2 touchPosition = new Vector2(screen_world.x, screen_world.y);
+
+        //listens for all interactions between the touch position and the box colliders
+        for (int i = 0; i < colliderlist.Count; i++)
+        {
+            ButtonControls buttonInspector = buttonArray[i].GetComponent<ButtonControls>();
+            Collider2D temp = colliderlist[i];
+            if (temp == Physics2D.OverlapPoint(touchPosition))
+            {
+                visibleDebug.text = "hit a button";
+                Debug.Log("hit button" + buttonArray[i].name);
+                if (curButton != -1)    //if this isnt the first button we have clicked
+                {
+                    visibleDebug.text = "we have hit another button";
+                    //check to make sure that the only buttons we interact with are ones that are linked
+                    for (int j = 0; j < curInteractableButtons.Count; j++)
+                    {
+                        if (buttonInspector.number == curInteractableButtons[j])
+                        {
+                            charInput.text += buttonInspector.curChar;
+                            //we hit a button that we can touch
+                        }
+                    }
+                }
+                else
+                {
+                    visibleDebug.text = "we hit our first button";
+                    curInteractableButtons = buttonInspector.interactableButtons;
+                    charInput.text = buttonInspector.curChar;
+                    curButton = buttonInspector.number;
+                    buttonInspector.sendClickedMessage();
+                }
+            }
+        }
+        /*if (t.phase == TouchPhase.Ended)
+        { //if the touch has ended
+
+            isTouching = false;
+            charInput.text = "";
+            //checkTheString
+        }*/
     }        
 }
 
