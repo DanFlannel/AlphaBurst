39c0653e0a91c40877dc727919d481736bcf922c
lmbradford115 <lm.bradford115@gmail.com>
2015-11-17 16:35:28 -0500

    added random letter generator

--- Assets/MyScripts/ButtonControls.cs
+++ Assets/MyScripts/ButtonControls.cs
@@ -9,11 +9,16 @@ public class ButtonControls : MonoBehaviour {
     public List<int> interactableButtons;
     public GameMaster gm;
     public string curChar;
+	//me 
+	public string st = "abcdefghijklmnopqrstuvwxyz";
 
 	// Use this for initialization
 	void Start () {
-        curChar = this.GetComponentInChildren<Text>().text;
+        //curChar = this.GetComponentInChildren<Text>().text;
+		//me 
+		curChar = st[Random.Range(0, st.Length)].ToString();
         gm = Camera.main.GetComponent<GameMaster>();
+		changeCharacter (curChar);
     }
 	
 	// Update is called once per frame
@@ -29,6 +34,8 @@ public class ButtonControls : MonoBehaviour {
     public void changeCharacter(string newChar)
     {
         curChar = newChar;
+		Text currentText = this.GetComponentInChildren<Text>();
+		currentText.text = newChar;
     }
 
 }
