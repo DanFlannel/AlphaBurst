5613a4fbfe78f6124b40b81437dbe7c1072c8408
Dan Flanagan <dan.flanagan93@gmail.com>
2015-12-01 17:15:05 -0500

    dictionary and score

--- Assets/MyScripts/TouchListener.cs
+++ Assets/MyScripts/TouchListener.cs
@@ -4,24 +4,34 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using System;
 using UnityEngine.EventSystems;
+using System.IO;
+using System.Text.RegularExpressions;
 
 public class TouchListener : MonoBehaviour {
 
     private GameMaster gm;
     public int curButton = -1;      //our current button number
     public Text charInput;          //the text that the button letters will be added to
+    public Text score;
+    public int points;
 
     private List<int> curInteractableButtons = new List<int>();         //list of interactable buttons as integers
 	private List<int> resetInteractions = new List<int>(new int[] {0,1,2,3,4,5,6,7,8}); //list of all buttons
+
+    public TextAsset dictionary;
+    private string[] dictionaryLines;
 
     public bool isInGame = true;
 
 	// Use this for initialization
 	void Start () {
+        score.text = "0";
+        points = 0;
         Input.simulateMouseWithTouches = true;              //makes the touches as if they were mouse touches
         charInput.text = "";                                //sets the input text to nothing
         gm = Camera.main.GetComponent<GameMaster>();
-	}
+        dictionaryLines = Regex.Split(dictionary.text, "\n");
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -29,6 +39,14 @@ public class TouchListener : MonoBehaviour {
         if (Input.GetMouseButtonUp(0))
         {
             curInteractableButtons = resetInteractions; //reset the ineractions so when we touch again we can touch any buttons
+            bool isWord = check_Dictionary(charInput.text);
+            if (isWord)
+            {
+                int pointsToAdd = addScore(charInput.text);
+                points += pointsToAdd;
+                score.text = points.ToString();
+                //TODO Add to the score
+            }
             charInput.text = "";                        //reset the text
         }
     }
@@ -82,6 +100,72 @@ public class TouchListener : MonoBehaviour {
         ButtonControls bc;
         bc = GameObject.Find("Letter_" + n).GetComponent<ButtonControls>();
         return bc;
-    }      
+    } 
+    
+    /// <summary>
+    /// checks the dictionary for the words being passed in
+    /// </summary>
+    private bool check_Dictionary(string word)
+    {
+        Debug.Log("called check dictionary");
+        Debug.Log(word);
+        Debug.Log("word length: " + word.Length);
+        char[] wordChars = word.ToCharArray();
+        Debug.Log(wordChars.ToString());
+        for(int i = 0; i < dictionaryLines.Length; i++)
+        {
+            //check for length
+            if (dictionaryLines[i].Length != word.Length + 1)   //not the same length
+            {
+                //wrong!
+            }
+            else    //they are the same length
+            {
+                //checking the first characters of the words
+                string lower = dictionaryLines[i].ToLower();
+                //Debug.Log(lower);
+                char[] temp = lower.ToCharArray();
+                if(temp[0] == wordChars[0])     //the words have the same first character and length
+                {
+                    int count = 0;
+                    for(int j = 0; j < word.Length; j++)
+                    {
+                        if(temp[j] != word[j])  //if the letters dont match stop checking the loop
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            count++;
+                        }
+                    }
+                    if(count == word.Length)
+                    {
+                        Debug.Log("found :" + word + " equalling " + dictionaryLines[i]);
+                        return true;
+                    }
+                }
+                else //the words have different first characters
+                {
+                    
+                }
+            }
+        }
+        return false;
+    }     
+
+    /// <summary>
+    /// simple factorial score adder
+    /// </summary>
+    private int addScore(string word)
+    {
+        int score = 0;
+        int length = word.Length;
+        for(int i = 0; i < word.Length; i++)
+        {
+            score += (i + 1) * 10;
+        }
+        return score;
+    }
 }
 
