9a40f1ae26577871d89129d4eae3a0adc0a56b67
Dan Flanagan <dan.flanagan93@gmail.com>
2016-07-06 18:42:01 -0400

    redid UI and scenes, imported packages
    imported beautiful transitions, reimported gpg, imported FatBobbyScene

---
+++ Assets/MyScripts/gpg_constants.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gpg_constants : MonoBehaviour {
+
+    public static string leaderboard_3x3_30_seconds = "";
+    public static string leaderboard_3x3_1_minute = "";
+    public static string leaderboard_3x3_2_minutes = "";
+
+    public static string leaderboard_4x4_1_minute = "";
+    public static string leaderboard_4x4_2_minutes = "";
+    public static string leaderboard_4x4_3_minutes = "";
+}
